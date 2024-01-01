42e309ecc3d2c10e34344cd5d806e46794511f1c
Ihopenot <984430652@qq.com>
2024-01-01 11:48:35 +0800

    Chore: make main.py more robust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 datapath = "./data"
 rawdatapath = "./rawdata"
@@ -16,11 +17,18 @@ if __name__ == "__main__":
 
     print(f"creating directory of {year}-{month}")
     input("Press Enter to comfirm...")
+
+    if os.path.exists(f"{datapath}/{year}/{month}"):
+        shutil.rmtree(f"{datapath}/{year}/{month}")
     os.makedirs(f"{datapath}/{year}/{month}")
+
     open(f"{datapath}/{year}/{month}/_.bean", "w").write(template)
     open(f"{datapath}/{year}/{month}/total.bean", "w")
     open(f"{datapath}/{year}/{month}/others.bean", "w")
-    open(f"{datapath}/{year}/_.bean", "a").write(f'include "{month}/_.bean"\n')
+    
+    newline = f'include "{month}/_.bean"\n'
+    if open(f"{datapath}/{year}/_.bean").read().find(newline) == -1:
+        open(f"{datapath}/{year}/_.bean", "a").write(newline)
 
     input("Create done, press Enter to import...")
     os.system(
