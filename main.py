bd31c3ac1234a08fd9caaaff1a6214b5c3ebb737
Ihopenot <984430652@qq.com>
2024-05-02 10:32:19 +0800

    feat: auto generate balace assertion

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import shutil
+from china_bean_importers.config import balance_accounts
 
-datapath = "./data"
-rawdatapath = "./rawdata"
-archivepath = "./archive"
+datapath = "data"
+rawdatapath = "rawdata"
+archivepath = "archive"
 
 template = """include "others.bean"
 include "total.bean"
@@ -18,13 +19,20 @@ if __name__ == "__main__":
     print(f"creating directory of {year}-{month}")
     input("Press Enter to comfirm...")
 
+    if not os.path.exists(f"{datapath}/{year}"):
+        os.makedirs(f"{datapath}/{year}")
+        open(f"{datapath}/{year}/_.bean", "w").write("\n")
+        newline = f'include "{datapath}/{year}/_.bean"\n'
+        if open(f"main.bean").read().find(newline) == -1:
+            open(f"main.bean", "a").write(newline)
+
     if os.path.exists(f"{datapath}/{year}/{month}"):
         shutil.rmtree(f"{datapath}/{year}/{month}")
     os.makedirs(f"{datapath}/{year}/{month}")
 
     open(f"{datapath}/{year}/{month}/_.bean", "w").write(template)
     open(f"{datapath}/{year}/{month}/total.bean", "w")
-    open(f"{datapath}/{year}/{month}/others.bean", "w")
+    others = open(f"{datapath}/{year}/{month}/others.bean", "w")
     
     newline = f'include "{month}/_.bean"\n'
     if open(f"{datapath}/{year}/_.bean").read().find(newline) == -1:
@@ -34,6 +42,12 @@ if __name__ == "__main__":
     os.system(
         f"bean-extract config.py {rawdatapath} -- {datapath}/{year}/{month}/total.bean"
     )
+
+    print("Input balance")
+    for account in balance_accounts:
+        balance = input(f"{account}: ")
+        others.write(f"{year}-{month}-01 balance {account} {balance} CNY\n")
+    others.close()
 
     input("Import done, press Enter to archive...")
     os.system(f"bean-file -o {archivepath} config.py {rawdatapath}")
