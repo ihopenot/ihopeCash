5339c77db27e2bd13306186cc01efc733bdf0063
Ihopenot <984430652@qq.com>
2024-09-01 16:51:55 +0800

    feat: auto download email attachment and decrypt

---
+++ mail.py
@@ -0,0 +1,179 @@
+import os, io, re, fitz, pyzipper
+import requests
+import imaplib
+import email
+from email.header import decode_header
+from typing import List, Tuple
+from config import Config
+import pandas as pd
+import itertools
+
+def decode_str(s):
+    val, encoding = decode_header(s)[0]
+    if encoding:
+        val = val.decode(encoding)
+    return val
+
+class BaseEmailHanlder:
+    def init(self, config):
+        pass
+
+    # we already filtered the email on the server side
+    def is_match(self, msg):
+        raise NotImplementedError
+
+    def get_data(self, msg) -> Tuple[str, bytes]:
+        for part in msg.walk():
+            file_name = part.get_filename()
+            if file_name:
+                name = decode_str(file_name)
+                data = part.get_payload(decode=True)
+                return name, data
+
+    def post_process(self, name: str, data: bytes) -> Tuple[str, bytes]:
+        return name, data
+
+    def savebill(self, name: str, data: bytes):
+        with open(os.path.join(Config["rawdata_path"], name), "wb") as f:
+            f.write(data)
+    
+    # just download the attachment
+    def process(self, msg):
+        if self.is_match(msg):
+            name, data = self.get_data(msg)
+            name, data = self.post_process(name, data)
+            self.savebill(name, data)
+
+
+def decrypt_zip(data, password_itr, extract_suffix=".csv", zipfile_cls=pyzipper.ZipFile):
+    with zipfile_cls(io.BytesIO(data)) as zip_ref:
+        for zip_info in zip_ref.infolist():
+            if zip_info.filename.endswith(extract_suffix):
+                for password in Config["passwords"]:
+                    try:
+                        with zip_ref.open(zip_info, pwd=password.encode()) as source:
+                            data = source.read()
+                            print(f'Successfully extracted .csv files from {password.encode()}')
+                            return zip_info.filename, data
+                    except Exception as e:
+                        pass
+
+                for password in password_itr:
+                    try:
+                        with zip_ref.open(zip_info, pwd=bytes(password)) as source:
+                            data = source.read()
+                            print(f'Successfully extracted .csv files from {bytes(password)}')
+                            return zip_info.filename, data
+                    except Exception as e:
+                        pass
+    return None
+
+
+class WeChatEmailHandler(BaseEmailHanlder):
+    reg_download_url = re.compile(r'"(https://download.bill.weixin.qq.com/cgi-bin/download_bill.cgi\?.*?)"')
+    password_itr = itertools.product(b"1234567890", repeat=6)
+
+    def is_match(self, msg):
+        subject = decode_str(msg["Subject"])
+        return "微信支付" in subject
+
+    def get_data(self, msg) -> Tuple[str, bytes]:
+        for part in msg.walk():
+            file_name = part.get_filename()
+            if not file_name and not part.is_multipart():
+                download_urls = self.reg_download_url.findall(part.get_payload(decode=True).decode("utf-8"))
+                if len(download_urls) > 0:
+                    r = requests.get(download_urls[0])
+                    return "wechat.zip", r.content
+    
+    def post_process(self, name: str, data: bytes) -> Tuple[str, bytes]:
+        return decrypt_zip(data, self.password_itr)
+
+
+
+class AlipayEmailHandler(BaseEmailHanlder):
+    password_itr = itertools.product(b"1234567890", repeat=6)
+
+    def is_match(self, msg):
+        subject = decode_str(msg["Subject"])
+        return "支付宝交易流水明细" in subject
+
+    def post_process(self, name: str, data: bytes) -> Tuple[str, bytes]:
+        return decrypt_zip(data, self.password_itr)
+
+
+class BoCDebitEmailHandler(BaseEmailHanlder):
+    password_itr = itertools.product("1234567890", repeat=6)
+
+    def is_match(self, msg):
+        subject = decode_str(msg["Subject"])
+        return "中国银行交易流水" in subject
+
+    def post_process(self, name: str, data: bytes) -> Tuple[str, bytes]:
+        try:
+            with fitz.open(stream=data, filetype="pdf") as doc:
+                if doc.is_encrypted:
+                    for password in Config["passwords"]:
+                        doc.authenticate(password)
+                        if not doc.is_encrypted:
+                            break
+                    if doc.is_encrypted:
+                        for password in self.password_itr:
+                            doc.authenticate("".join(password))
+                            if not doc.is_encrypted:
+                                break
+                    if doc.is_encrypted:
+                        raise Exception('Failed to decrypt')
+                    print(f'Successfully decrypted {name}')
+                    return name, doc.tobytes()
+        except Exception as e:
+            print(e)
+
+class CCBDebitEmailHandler(BaseEmailHanlder):
+    password_itr = itertools.product(b"1234567890", repeat=6)
+
+    def is_match(self, msg):
+        subject = decode_str(msg["Subject"])
+        return "中国建设银行个人活期账户交易明细" in subject
+
+    def post_process(self, name: str, data: bytes) -> Tuple[str, bytes]:
+        name, xlsfile = decrypt_zip(data, self.password_itr, ".xls", pyzipper.AESZipFile)
+        xls_df = pd.read_excel(io.BytesIO(xlsfile))
+        name = os.path.splitext(name)[0] + ".csv"
+        return name, xls_df.to_csv(None, index=False).encode()
+
+handlers: List[BaseEmailHanlder] = [
+    WeChatEmailHandler(),
+    AlipayEmailHandler(),
+    BoCDebitEmailHandler(),
+    CCBDebitEmailHandler(),
+]
+
+def DownloadFiles():
+    cfg = Config["email"]["imap"]
+    server = imaplib.IMAP4_SSL(cfg["host"], cfg["port"])
+    server.login(cfg["username"], cfg["password"])
+
+    # server.create("Bills")
+    print(server.list())
+    print(server.select(cfg["mailbox"]))
+    _, mails = server.search(None, 'UNSEEN')
+    print(mails)
+    mails = mails[0].split()
+    for mail in mails:
+        _, data = server.fetch(mail, '(RFC822)')
+        msg = email.message_from_bytes(data[0][1])
+
+        for handler in handlers:
+            handler.process(msg)
+
+        server.store(mail, '+FLAGS', '\\Seen')
+
+    server.logout()
+
+
+DownloadFiles()
+# decrypt_zip("rawdata/wechat.zip", itertools.product(b"1234567890", repeat=6))
+# WeChatEmailHandler().post_process("wechat.zip", open("rawdata/wechat.zip", "rb").read())
+# BoCDebitEmailHandler().post_process("KA020000001560066560001.pdf", open("rawdata/KA020000001560066560001.pdf", "rb").read())
+# CCBDebitEmailHandler().post_process("hqmx_20240901131340.zip", open("rawdata/hqmx_20240901131340.zip", "rb").read())
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import zipfile
+from mail import DownloadFiles
 import fitz
 import pandas as pd
 from config import Config
@@ -13,61 +13,10 @@ template = """include "others.bean"
 include "total.bean"
 """
 
-def decrypt_rawdata(remove_origin=True):
-    for filename in os.listdir(rawdatapath):
-        filepath = os.path.join(rawdatapath, filename)
-        
-        # 获取文件名（不包含后缀）
-        file_basename = os.path.splitext(filename)[0]
-        
-        # 如果是.zip文件，尝试解压
-        if filename.endswith('.zip'):
-            try:
-                with zipfile.ZipFile(filepath, 'r') as zip_ref:
-                    for zip_info in zip_ref.infolist():
-                        if zip_info.filename.endswith('.csv'):
-                            extracted_path = os.path.join(rawdatapath, os.path.basename(zip_info.filename))
-                            with zip_ref.open(zip_info, pwd=file_basename.encode()) as source, open(extracted_path, 'wb') as target:
-                                target.write(source.read())
-                print(f'Successfully extracted .csv files from {filename}')
-                if remove_origin:
-                    os.remove(filepath)
-            except Exception as e:
-                print(f'Failed to extract .csv files from {filename}: {e}')
-        
-        # 如果是.pdf文件，尝试解密
-        elif filename.endswith('.pdf'):
-            try:
-                is_encrypted = False
-                with fitz.open(filepath) as doc:
-                    if doc.is_encrypted:
-                        is_encrypted = True
-                        doc.authenticate(file_basename)
-                        if doc.is_encrypted:
-                            raise Exception('Failed to decrypt')
-                        decrypted_filepath = os.path.join(rawdatapath, f'decrypted_{filename}')
-                        doc.save(decrypted_filepath)
-                        print(f'Successfully decrypted {filename}')
-                if remove_origin and is_encrypted:
-                    os.remove(filepath)
-            except Exception as e:
-                print(f'Failed to decrypt {filename}: {e}')
-        if filename.endswith('.xls'):
-            try:
-                # 读取 .xls 文件并转换为 .csv 文件
-                xls_df = pd.read_excel(filepath)
-                extracted_path = os.path.join(rawdatapath, file_basename) + ".csv"
-                xls_df.to_csv(extracted_path, index=False)
-                print(f'Successfully converted {filename} to .csv')
-                if remove_origin:
-                    os.remove(filepath)
-            except Exception as e:
-                print(f'Failed to convert {filename} to .csv: {e}')
-
 if __name__ == "__main__":
-    opt = input("Decrypting rawdata, Do you want to remove the original files? (Y/n)")
-    rm_ori = False if len(opt) > 0 and opt[0].lower() == 'n' else True
-    decrypt_rawdata(rm_ori)
+    input("Downloading bills, press Enter to continue...")
+    DownloadFiles()
+    # decrypt_rawdata(rm_ori)
     input("decryption done, press Enter to continue...")
 
     year = input("Year: ")
@@ -103,7 +52,7 @@ if __name__ == "__main__":
     )
 
     print("Input balance")
-    for account in balance_accounts:
+    for account in Config["balance_accounts"]:
         balance = input(f"{account}: ")
         others.write(f"{year}-{str(month+1).rjust(2, "0")}-01 balance {account} {balance} CNY\n")
     others.close()
